4f025888e65ea90ef5e2a02e5ed0ad5bac039727
Guillaume-Papuga <guillaume.papuga@gmail.com>
2023-11-29 18:36:37 +0100

    Cleaning the two datasets

--- 02_import_data.R
+++ 02_import_data.R
@@ -40,21 +40,42 @@ med_basin_txt = sf::st_geometry(med_basin_simpl) %>%
 
 # Import
 # Save Raw and start from computer file
-occ_download_prep() # preview the download request before sending to GBIF
-
+# occ_download_prep() # preview the download request before sending to GBIF
 plant_med = occ_download(pred("taxonKey", 2858200),
-                         pred_within(med_basin_txt),
+                         #pred_lte("coordinateUncertaintyInMeters",1000), # key is less than/equal value
+                         pred_in("datasetKey", c("7a3679ef-5582-4aaa-81f0-8c2545cafc81",  # plantnet
+                                                 "50c9509d-22c7-4a22-a47d-8c48425ef4a7")),  # inaturalist
+                         pred_gte("year", 2000), # set the starting year
+                         pred_within(med_basin_txt), # lat-lon values within WKT polygon
+                         pred("hasGeospatialIssue", FALSE), #   Remove default geospatial issues.
+                         pred("hasCoordinate", TRUE), #   Keep only records with coordinates.
+                         pred_gte("distanceFromCentroidInMeters","2000"), # filter out country/area centroids in a download
+                         pred("occurrenceStatus","PRESENT"), #   Remove absent records.
+                         pred_not(pred_in("basisOfRecord",c("FOSSIL_SPECIMEN"))), #   Remove fossils and living specimens
                          format = "SIMPLE_CSV")
 
 occ_download_wait(plant_med) # check status
+gbif_citation(occ_download_get(plant_med)) # get the correct GBIF citation of the dataset
 
-data_plant_med = occ_download_get(plant_med) %>%
-  occ_download_import()
+# Cleaning pipeline
+data_plant_med = plant_med %>%
+  occ_download_get(overwrite=TRUE) %>% # GBIF get
+  occ_download_import() %>% # GBIF import in R
+  setNames(tolower(names(.))) %>% # set lowercase column names to work with CoordinateCleaner
+  filter(occurrencestatus  == "PRESENT") %>% # delete ABSENCE data
+  filter(coordinateuncertaintyinmeters < 2000 | is.na(coordinateuncertaintyinmeters)) %>% # keep low uncertainty (keep NA for now)
+  filter(!coordinateuncertaintyinmeters %in% c(301,3036,999,9999)) %>% # remove records with known default values for coordinateUncertaintyInMeters. These can be GeoLocate centroids or some other default. It is good to remove them because usually the uncertainy is larger than what is stated by the value
+  cc_cen(buffer = 1000, lon = "decimallongitude", lat = "decimallatitude") %>% # remove country centroids within 2km 
+  cc_cap(buffer = 1000, lon = "decimallongitude", lat = "decimallatitude") %>% # remove capitals centroids within 2km
+  cc_inst(buffer = 1000, lon = "decimallongitude", lat = "decimallatitude") %>% # remove zoo and herbaria within 2km 
+  cc_sea(lon = "decimallongitude", lat = "decimallatitude", ref = "buffland", value = "flagged") %>% # remove from ocean 
+  distinct(decimallatitude, decimallongitude, specieskey, .keep_all = TRUE) # delete data if copied in the two datasets
 
-write.csv(data_plant_med, 
-          here::here("data", "raw", "narcissus.csv"))
-
-# Clean
+# Select columns
+data_plant_med = data_plant_med %>%
+  select() %>% # select appropriate columns
+  mutate () # highlight the two datasets
 
 # Save
-
+write.csv(data_plant_med, 
+          here::here("data", "raw", "narcissus.csv"))
